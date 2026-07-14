0f22c7579b7964ac5ead53e5581335f065e1c399
Lucas Boeykens <lucas.boeykens@telenet.be>
2026-07-14 11:29:56 +0200

    added shell scripts to submit the MuSA run. Note that these are experimental and may be removed

---
+++ wrapper_submitJobs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+cluster=${1:-"doduo"}
+
+ntiles=44
+#read in the tiles
+rootdirMuSAruns="/kyukon/data/gent/vo/000/gvo00090/vsc44965/Doctoraat/Python/Machine_learning/Traditional_MLA/sd/MuSA/test/"
+
+scriptdir="$(dirname "$0")"
+jobscript="/kyukon/data/gent/vo/000/gvo00090/vsc44965/Doctoraat/Python/Machine_learning/Traditional_MLA/sd/MuSA/runMuSAtile.sh"
+
+
+job=$(sbatch --job-name=test_MuSArun \
+  --output=logs/output_test_MuSArun_%A.log \
+  --error=logs/error_test_MuSArun_%A.log \
+  --nodes=1 \
+  --cpus-per-task=12 \
+  --time="01:00:00" \
+  --cluster=${cluster} \
+  --array=1-1 \
+  --wrap="$jobscript \${SLURM_ARRAY_TASK_ID} \
+                        $rootdirMuSAruns")
+
+#--array=0-$((ntiles-1)) \
